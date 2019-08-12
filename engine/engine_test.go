77ed313df22006cad6c22d9088d82cb53e21b350
Jeremy Kitchen <kitchen@kitchen.io>
2019-08-12 11:17:01 -0700

    work on porting engine tests to a suite

--- engine/engine_test.go
+++ engine/engine_test.go
@@ -1,152 +1,255 @@
 package engine
 
-import "testing"
-import "github.com/stretchr/testify/assert"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
 
 func (engine *engine) setFakeValue(key string) {
 	engine.storage[key] = &fakeValueStore{}
 }
 
-func TestNewEngine(t *testing.T) {
-	var bigEngine Engine
-	bigEngine = NewEngine()
-	assert.NotNil(t, bigEngine)
-
-	var littleEngine *engine
-	littleEngine = newEngine()
-	assert.NotNil(t, littleEngine)
+type engineTestSuite struct {
+	suite.Suite
+	engine engine
 }
 
-func TestEngineIncr(t *testing.T) {
-	engine := newEngine()
-	value, err := engine.Incr("abc")
-	assert.Equal(t, int64(1), value)
-	assert.NoError(t, err)
+func (suite *engineTestSuite) SetupTest() {
+	suite.engine = *newEngine()
+	// TODO: turn all of these strings into constants
+	suite.engine.Set("intvalue", "1234567890")
+	suite.engine.Set("stringvalue", "foobartestvalueaoeuhtns")
+	suite.engine.setFakeValue("fakevalue")
+}
 
-	value, err = engine.Incr("abc")
-	assert.Equal(t, int64(2), value)
-	assert.NoError(t, err)
+func (suite *engineTestSuite) TestNewEngine() {
+	var interfaceEngine Engine
+	interfaceEngine = NewEngine()
+	suite.NotNil(interfaceEngine)
 
-	value, err = engine.Decr("def")
-	assert.Equal(t, int64(-1), value)
-	assert.NoError(t, err)
+	var rawEngine engine
+	rawEngine = *newEngine()
+	suite.NotNil(rawEngine)
+}
 
-	value, err = engine.Decr("def")
-	assert.Equal(t, int64(-2), value)
-	assert.NoError(t, err)
+func (suite *engineTestSuite) TestGetSetStringValues() {
+	value, err := suite.engine.Get("stringvalue")
+	suite.NoError(err)
+	suite.Equal("foobartestvalueaoeuhtns", *value)
 
-	value, err = engine.Incrby("foo", "10")
-	assert.Equal(t, int64(10), value)
-	assert.NoError(t, err)
+	err = suite.engine.Set("key", "abc")
+	suite.NoError(err)
 
-	value, err = engine.Incrby("foo", "10")
-	assert.Equal(t, int64(20), value)
-	assert.NoError(t, err)
+	value, err = suite.engine.Get("key")
+	suite.NoError(err)
+	suite.Equal("abc", *value)
+}
 
-	value, err = engine.Decrby("bar", "10")
-	assert.Equal(t, int64(-10), value)
-	assert.NoError(t, err)
+func (suite *engineTestSuite) TestGetSetIntValues() {
+	value, err := suite.engine.Get("intvalue")
+	suite.NoError(err)
+	suite.Equal("1234567890", *value)
 
-	value, err = engine.Decrby("bar", "10")
-	assert.Equal(t, int64(-20), value)
-	assert.NoError(t, err)
+	err = suite.engine.Set("key", "123")
+	suite.NoError(err)
 
-	_, err = engine.Incrby("aoeuhtns", "aoeuhtns")
-	assert.Error(t, err)
+	value, err = suite.engine.Get("key")
+	suite.NoError(err)
+	suite.Equal("123", *value)
+}
 
-	_, err = engine.Decrby("aoeuhtns", "aoeuhtns")
-	assert.Error(t, err)
+func (suite *engineTestSuite) TestGetOtherValues() {
+	value, err := suite.engine.Get("doesnotexist")
+	suite.NoError(err)
+	suite.Nil(value)
+
+	_, err = suite.engine.Get("fakevalue")
+	suite.Error(err)
+}
+
+func (suite *engineTestSuite) TestGetSetMethod() {
+	value, err := suite.engine.GetSet("intvalue", "9876")
+	suite.NoError(err)
+	suite.Equal("1234567890", *value)
+
+	value, err = suite.engine.Get("intvalue")
+	suite.NoError(err)
+	suite.Equal("9876", *value)
+
+	value, err = suite.engine.GetSet("stringvalue", "abc123")
+	suite.NoError(err)
+	suite.Equal("foobartestvalueaoeuhtns", *value)
+
+	value, err = suite.engine.Get("stringvalue")
+	suite.NoError(err)
+	suite.Equal("abc123", *value)
+
+	value, err = suite.engine.GetSet("doesnotexist", "it does now")
+	suite.NoError(err)
+	suite.Nil(value)
+
+	value, err = suite.engine.Get("doesnotexist")
+	suite.NoError(err)
+	suite.Equal("it does now", *value)
+
+	_, err = suite.engine.GetSet("fakevalue", "fake fake fake")
+	suite.Error(err)
+
+	_, err = suite.engine.Get("fakevalue")
+	suite.Error(err)
+
+	typeString, err := suite.engine.Type("fakevalue")
+	suite.NoError(err)
+	suite.Equal("fake", typeString)
+}
+
+func (suite *engineTestSuite) TestDel() {
+	deleted, err := suite.engine.Del([]string{"stringvalue", "intvalue", "doesnotexist"})
+	suite.NoError(err)
+	suite.Equal(int64(2), deleted)
+
+	value, err := suite.engine.Get("stringvalue")
+	suite.NoError(err)
+	suite.Nil(value)
+
+	value, err = suite.engine.Get("intvalue")
+	suite.NoError(err)
+	suite.Nil(value)
+
+	value, err = suite.engine.Get("doesnotexist")
+	suite.NoError(err)
+	suite.Nil(value)
+}
+
+func (suite *engineTestSuite) TestExists() {
+	exists, err := suite.engine.Exists([]string{"stringvalue", "intvalue", "doesnotexist"})
+	suite.NoError(err)
+	suite.Equal(int64(2), exists)
+}
+
+func (suite *engineTestSuite) TestStrLen() {
+	length, err := suite.engine.Strlen("stringvalue")
+	suite.NoError(err)
+	suite.Equal(int64(23), length)
+
+	length, err = suite.engine.Strlen("intvalue")
+	suite.NoError(err)
+	suite.Equal(int64(10), length)
+
+	length, err = suite.engine.Strlen("doesnotexist")
+	suite.NoError(err)
+	suite.Equal(int64(0), length)
+
+	_, err = suite.engine.Strlen("fakevalue")
+	suite.Error(err)
+}
+
+func (suite *engineTestSuite) TestIntIncrDecr() {
+	err := suite.engine.Set("intvalue", "1")
+	suite.NoError(err)
+
+	value, err := suite.engine.Incr("intvalue")
+	suite.NoError(err)
+	suite.Equal(int64(2), value)
+
+	// TODO: make Incrby and Decrby use int64 instead of string? :|
+	value, err = suite.engine.Incrby("intvalue", "10")
+	suite.NoError(err)
+	suite.Equal(int64(12), value)
+
+	value, err = suite.engine.Decr("intvalue")
+	suite.NoError(err)
+	suite.Equal(int64(11), value)
+
+	value, err = suite.engine.Decrby("intvalue", "100")
+	suite.NoError(err)
+	suite.Equal(int64(-89), value)
+
+	value, err = suite.engine.Incr("incr")
+	suite.NoError(err)
+	suite.Equal(int64(1), value)
+
+	value, err = suite.engine.Incrby("incrby", "10")
+	suite.NoError(err)
+	suite.Equal(int64(10), value)
+
+	value, err = suite.engine.Decr("decr")
+	suite.NoError(err)
+	suite.Equal(int64(-1), value)
+
+	value, err = suite.engine.Decrby("decrby", "10")
+	suite.NoError(err)
+	suite.Equal(int64(-10), value)
+}
+
+func (suite *engineTestSuite) TestOtherIncrDecr() {
+	_, err := suite.engine.Incr("stringvalue")
+	suite.Error(err)
+
+	_, err = suite.engine.Incrby("stringvalue", "10")
+	suite.Error(err)
+
+	_, err = suite.engine.Decr("stringvalue")
+	suite.Error(err)
+
+	_, err = suite.engine.Decrby("stringvalue", "10")
+	suite.Error(err)
+
+	// make sure we haven't modified the string value anywhere in there
+	value, err := suite.engine.Get("stringvalue")
+	suite.NoError(err)
+	suite.Equal("foobartestvalueaoeuhtns", *value)
+
+	_, err = suite.engine.Incrby("intvalue", "invalidint")
+	suite.Error(err)
+
+	_, err = suite.engine.Decrby("intvalue", "anotherinvalidint")
+	suite.Error(err)
+
+	// make sure we haven't modified the int value anywhere in there
+	value, err = suite.engine.Get("intvalue")
+	suite.NoError(err)
+	suite.Equal("1234567890", *value)
+
+	_, err = suite.engine.Incr("fakevalue")
+	suite.Error(err)
+
+	// TODO: there's some subtlety here in whether the string parsing fails or
+	// the key is of the wrong type (string, fakevalue, etc) that could use some assertions
+	// but right now we're not sending specific error types so we'll ignore it
+	_, err = suite.engine.Incrby("fakevalue", "10")
+	suite.Error(err)
+
+	_, err = suite.engine.Decr("fakevalue")
+	suite.Error(err)
+
+	_, err = suite.engine.Decrby("fakevalue", "10")
+	suite.Error(err)
+}
+
+func (suite *engineTestSuite) TestMGet() {
 
 }
 
-func TestGetSetDelExists(t *testing.T) {
-	engine := newEngine()
-	err := engine.Set("abc", "foo")
-	assert.NoError(t, err)
-	value, err := engine.Get("abc")
-	assert.Equal(t, "foo", *value)
-	assert.NoError(t, err)
+func (suite *engineTestSuite) TestMSet() {
 
-	value, err = engine.Get("abc")
-	assert.NoError(t, err)
-	assert.Equal(t, "foo", *value)
-
-	err = engine.Set("abc", "123")
-	assert.NoError(t, err)
-
-	value, err = engine.Get("abc")
-	assert.Equal(t, "123", *value)
-	assert.NoError(t, err)
-
-	value, err = engine.Get("abc")
-	assert.NoError(t, err)
-	assert.Equal(t, "123", *value)
-
-	err = engine.Set("key1", "abc")
-	assert.NoError(t, err)
-	err = engine.Set("key2", "abc")
-	assert.NoError(t, err)
-	err = engine.Set("key3", "abc")
-	assert.NoError(t, err)
-
-	exists, err := engine.Exists([]string{"key1", "key2", "key3"})
-	assert.Equal(t, int64(3), exists)
-	assert.NoError(t, err)
-
-	exists, err = engine.Exists([]string{"doesnotexist"})
-	assert.Equal(t, int64(0), exists)
-	assert.NoError(t, err)
-
-	deleted, err := engine.Del([]string{"key1", "key2", "key3", "doesnotexist"})
-	assert.NoError(t, err)
-	assert.Equal(t, int64(3), deleted)
-
-	deleted, err = engine.Del([]string{"anotherdoesnotexist"})
-	assert.NoError(t, err)
-	assert.Equal(t, int64(0), deleted)
-
-	value, err = engine.Get("doesnotexist")
-	assert.Nil(t, value)
-	assert.NoError(t, err)
-
-	value, err = engine.GetSet("getset", "abc")
-	assert.Nil(t, value)
-	assert.NoError(t, err)
-
-	value, err = engine.GetSet("getset", "def")
-	assert.Equal(t, "abc", *value)
-	assert.NoError(t, err)
-
-	value, err = engine.Get("getset")
-	assert.Equal(t, "def", *value)
-	assert.NoError(t, err)
-
-	engine.setFakeValue("fake")
-	_, err = engine.GetSet("fake", "fake")
-	assert.Error(t, err)
 }
 
-func TestStrLen(t *testing.T) {
-	engine := newEngine()
+func (suite *engineTestSuite) TestType() {
 
-	engine.Set("stringvalue", "aoeuhtns")
-	value, err := engine.Strlen("stringvalue")
-	assert.NoError(t, err)
-	assert.Equal(t, int64(8), value)
+}
 
-	engine.Set("intvalue", "1234567890")
-	value, err = engine.Strlen("intvalue")
-	assert.NoError(t, err)
-	assert.Equal(t, int64(10), value)
+func (suite *engineTestSuite) TestInternalMethods() {
+	// getOrDefault
+	// getStore
+	// set
+	// del
+}
 
-	value, err = engine.Strlen("doesnotexist")
-	assert.Equal(t, int64(0), value)
-	assert.NoError(t, err)
-
-	// TODO: test what happens when strlen on an invalid data type (e.g. fakeValueStore)
-	engine.setFakeValue("fake")
-	_, err = engine.Strlen("fake")
-	assert.Error(t, err)
+func TestEngineTestSuite(t *testing.T) {
+	suite.Run(t, new(engineTestSuite))
 }
 
 func TestMGet(t *testing.T) {
