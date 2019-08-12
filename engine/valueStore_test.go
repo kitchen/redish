f8dfa46d00394343ef8d488676995a837fb0bb25
Jeremy Kitchen <kitchen@kitchen.io>
2019-08-12 08:22:29 -0700

    convert valueStore tests to test suite
    I like this a lot because it gives me SetupTests(), makes it so I don't
    have to pass t all the time (though I'm prefixing suite variables with
    suite all the time.. shrug), and most importantly, namespaces the test
    cases so I don't have to think about method names

--- engine/valueStore_test.go
+++ engine/valueStore_test.go
@@ -3,7 +3,7 @@ package engine
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 type fakeValueStore struct {
@@ -14,13 +14,45 @@ func (s *fakeValueStore) getType() string {
 	return "fake"
 }
 
-func TestDefaultFailures(t *testing.T) {
-	store := fakeValueStore{}
-	_, err := store.get()
-	assert.Error(t, err)
-
-	_, err = store.incrby(1)
-	assert.Error(t, err)
-
-	assert.Equal(t, "fake", store.getType())
+type valueStoreTestSuite struct {
+	suite.Suite
+	store fakeValueStore
 }
+
+func (suite *valueStoreTestSuite) SetupTest() {
+	suite.store = fakeValueStore{}
+}
+
+func (suite *valueStoreTestSuite) TestDefaultFailures() {
+	_, err := suite.store.get()
+	suite.Error(err)
+
+	_, err = suite.store.incrby(1)
+	suite.Error(err)
+}
+
+func (suite *valueStoreTestSuite) TestInterface() {
+	var store valueStoreInterface
+	store = &fakeValueStore{}
+	suite.NotNil(store)
+}
+
+func (suite *valueStoreTestSuite) TestDefaultPasses() {
+	suite.Equal("fake", suite.store.getType())
+}
+
+func TestValueStoreTestSuite(t *testing.T) {
+	suite.Run(t, new(valueStoreTestSuite))
+}
+
+//
+// func TestDefaultFailures(t *testing.T) {
+// 	store := fakeValueStore{}
+// 	_, err := store.get()
+// 	assert.Error(t, err)
+//
+// 	_, err = store.incrby(1)
+// 	assert.Error(t, err)
+//
+// 	assert.Equal(t, "fake", store.getType())
+// }
